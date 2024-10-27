b6dba4c5fd445c1f1736a54b74dddb8f4c9b5f3b
shb20tsinghua <160827516+shb20tsinghua@users.noreply.github.com>
2024-10-27 23:53:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import numpy as np
-from game import Game
+import torch
+if not (use_torch := False):
+    from game import Game
+    np.seterr(divide='ignore', invalid='ignore')
+else:
+    from game_torch import Game
+    torch.set_default_device(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    torch.set_default_dtype(torch.float64)
 from game_plot import GamePlot
 from multiprocessing import Pool
-np.seterr(divide='ignore')
 fresult, frecord = 'data/result.log', 'data/record.log'
 preci = 3
-def arr2str(arr): return np.array2string(arr, separator=',', formatter={'all': lambda x: f'{x:.{preci}e}'}).replace('\n', '').replace(' ', '')
+def arr2str(arr, preci): return np.array2string(arr, separator=',', formatter={'all': lambda x: f'{x:.{preci}e}'}).replace('\n', '').replace(' ', '')
 
 
-def test_static(seed):
-    np.random.seed(seed)
-    ua_static = np.random.rand(*((Na,)*Ni+(Ni,)))-0.5
-    gamma = 0
-    Ns = 1
-    ua, Ta = ua_static[..., np.newaxis, :], np.ones((Na,)*Ni+(1, 1))
-    np.random.seed()
-    init_policy = np.random.dirichlet(np.ones(Na), size=(Nn, Ns, Ni))
-    result, success, nit, record, record_csv = Game(Ns, Ni, Na, gamma, ua, Ta).solve(init_policy, frecord, verbose=1)
-    with open(fresult, 'a') as fio:
-        fio.writelines(f"|{seed:^4d}|{repr(success):7}|{nit:^7d}|{'|'.join([arr2str(item) for item in record])}|\n")
-    cano_sect, policy, value = result
-
-
-def test(seed):
+def test(seed, test_static=False, verbose=0):
+    global gamma
     np.random.seed(seed)
     ua, Ta = np.random.rand(*((Na,)*Ni+(Ns, Ni)))-0.5, np.random.dirichlet(np.ones(Ns), size=(Na,)*Ni+(Ns,))
-    np.random.seed()
+    # np.random.seed()
     init_policy = np.random.dirichlet(np.ones(Na), size=(Nn, Ns, Ni))
-    result, success, nit, record, record_csv = Game(Ns, Ni, Na, gamma, ua, Ta).solve(init_policy, frecord, verbose=1, plots=True)
+    if test_static:
+        gamma = 0
+        Ta = np.kron(np.ones((Na,)*Ni)[..., np.newaxis, np.newaxis], np.eye(Ns))
+    if use_torch:
+        ua, Ta, init_policy = [torch.from_numpy(item).to(torch.get_default_device()) for item in [ua, Ta, init_policy]]
+    result, success, nit, record, record_csv = Game(Ns, Ni, Na, gamma, ua, Ta).solve(init_policy, verbose=verbose, record_file=frecord, preci=preci, plots=True)
+    if use_torch:
+        result, record = [item.numpy() for item in result], [item.numpy() for item in record]
     if record_csv:
-        np.savetxt('data/record.csv', np.vstack(record_csv), delimiter=',')
+        np.savetxt('data/record.csv', np.vstack(record_csv) if not use_torch else torch.vstack(record_csv).numpy(), delimiter=',')
     with open(fresult, 'a') as fio:
         fio.writelines(f"|{seed:^4d}|{repr(success):7}|{nit:^7d}|{'|'.join([arr2str(item) for item in record])}|\n")
     cano_sect, policy, value = result
@@ -45,18 +45,18 @@ def plottest(seed):
 
 
 gamma = 0.5
-Nn, Ns, Ni, Na = 6, 2, 2, 2
+Nn, Ns, Ni, Na = 1, 3, 3, 3
 if __name__ == '__main__':
     print(f"Check results at {fresult} and iteration process at {frecord}.")
     open(fresult, 'w')
     with open(fresult, 'a') as fio:
         nlen = (6+preci)*Nn*Ns+(Ns-1)*Nn+2*Nn+Nn+1
         fio.writelines(f"|{'seed':^4}|{'success':^7}|{'nit':^7}|{'cano_sect':^{nlen}}|{'policy_bias':^{nlen}}|{'regret_bias':^{nlen}}|{'projgrad':^{nlen}}|{'dp_res':^{(6+preci)*Nn*Ni+(Ni-1)*Nn+2*Nn+Nn+1}}|{'beta':^{8+preci}}|\n")
-    if single_test := True:
-        seed = 1
-        test(seed)
+    if not (parallel_test := False):
+        seed = 5
+        test(seed, test_static=False, verbose=1)
         if Ns == 2 and Ni == 2 and Na == 2:
             plottest(seed)
     else:
         with Pool(processes=8) as pool:
-            pool.map(test, np.random.randint(2000, size=8), chunksize=1)
+            pool.map(test, range(2000), chunksize=1)
