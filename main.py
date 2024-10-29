1b5ee14a167a12403332889894ae106d2baf4b43
shb20tsinghua <160827516+shb20tsinghua@users.noreply.github.com>
2024-10-29 12:38:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
-if not (use_torch := False):
-    from game import Game
+if not (use_torch := True):
+    from game_numpy import Game
 else:
     from game_torch import Game
 from game_plot import GamePlot
@@ -11,7 +11,13 @@ preci = 3
 def arr2str(arr, preci): return np.array2string(arr, separator=',', formatter={'all': lambda x: f'{x:.{preci}e}'}).replace('\n', '').replace(' ', '')
 
 
-def test(seed, test_static=False, verbose=0):
+def test(seed, test_static=False, verbose=0, plots=True):
+    """
+        Parameters:
+        test_static: Whether to treat the input dynamic game as a set of static games.
+        varbose: If 0, outputs nothing, if 1, outputs the outer iteration level, if 2, outputs both two iteration levels.
+        plots: Whether to record every iteration step in a list, which can be used to animate the line search process later. (Only recorded for 2-state 2-player 2-action dynamic games.)
+    """
     global gamma
     np.random.seed(seed)
     ua, Ta = np.random.rand(*((Na,)*Ni+(Ns, Ni)))-0.5, np.random.dirichlet(np.ones(Ns), size=(Na,)*Ni+(Ns,))
@@ -20,7 +26,12 @@ def test(seed, test_static=False, verbose=0):
     if test_static:
         gamma = 0
         Ta = np.kron(np.ones((Na,)*Ni)[..., np.newaxis, np.newaxis], np.eye(Ns))
-    result, success, nit, record, record_csv = Game(Ns, Ni, Na, gamma, ua, Ta).solve(init_policy, verbose=verbose, record_file=frecord, preci=preci, plots=True)
+    result, success, nit, record, record_csv = Game(Ns, Ni, Na, gamma, ua, Ta).solve(
+        init_policy,
+        pdbias_TOL=1e-9, canosect_TOL=1e-5, resolu=1e-10,
+        canosect_stepln=2e-1, singuavoi_stepln=2e-1, singuavoi_stepln_decay=0.9, projgrad_stepln=2e-4, dynaprogr_stepln=1e-1,
+        maxnit=500000, maxsubnit=10000,
+        verbose=verbose, record_file=frecord, preci=preci, plots=plots)
     if record_csv is not None:
         np.savetxt('data/record.csv', record_csv, delimiter=',')
     with open(fresult, 'a') as fio:
@@ -44,12 +55,13 @@ if __name__ == '__main__':
     open(fresult, 'w')
     with open(fresult, 'a') as fio:
         nlen = (6+preci)*Nn*Ns+(Ns-1)*Nn+2*Nn+Nn+1
-        fio.writelines(f"|{'seed':^4}|{'success':^7}|{'nit':^7}|{'cano_sect':^{nlen}}|{'policy_bias':^{nlen}}|{'regret_bias':^{nlen}}|{'projgrad':^{nlen}}|{'dp_res':^{(6+preci)*Nn*Ni+(Ni-1)*Nn+2*Nn+Nn+1}}|{'beta':^{8+preci}}|\n")
+        fio.writelines(f"|{'seed':^4}|{'success':^7}|{'nit':^7}|{'cano_sect':^{nlen}}|{'policy_bias':^{nlen}}|{'regret_bias':^{nlen}}|{'projgrad':^{nlen}}|{'dp_res':^{(6+preci)*Nn*Ni+(Ni-1)*Nn+2*Nn+Nn+1}}|\n")
     if not (parallel_test := False):
         seed = 0
-        test(seed, test_static=False, verbose=1)
-        if Ns == 2 and Ni == 2 and Na == 2:
+        plots = False
+        test(seed, test_static=False, verbose=1, plots=plots)
+        if plots and Ns == 2 and Ni == 2 and Na == 2:
             plottest(seed)
     else:
         with Pool(processes=8) as pool:
-            pool.map(test, range(2000), chunksize=1)
+            pool.map(test, np.random.randint(2000, size=8), chunksize=1)
