629dcbcfd4c12c107951e4f9da772ab18c29a4f1
shb20tsinghua <160827516+shb20tsinghua@users.noreply.github.com>
2024-12-12 20:40:44 +0800

    Update game_torch.py

--- game_torch.py
+++ game_torch.py
@@ -1,5 +1,4 @@
 import torch
-syms = 'abcdefghjklmopqrtuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 torch.set_default_device(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 torch.set_default_dtype(torch.float64)
 def arr2str(arr): return str(arr)[7:-1].replace('\n', '').replace(' ', '')
@@ -28,91 +27,92 @@ class Game:
         self.max_value = ua.max()/(1-gamma)
         self.record = []
 
-    def solve(self, policy, pdbias_TOL=1e-6, canosect_TOL=1e-5, resolu=1e-10, canosect_stepln=2e-1, singuavoi_stepln=2e-1, singuavoi_stepln_decay=0.9, projgrad_stepln=2e-4, dynaprogr_stepln=1e-1, maxnit=500000, maxsubnit=10000, verbose=0, record_file=None, preci=3, plots=False):
+    def solve(self, policy, canosect_TOL=1e-5, maxnit=500000, canosect_stepln=2e-1, singuavoi_stepln=2e-1, verbose=0, record_file=None, preci=3, plots=False):
         """
         Parameters:
         policy: Initial policy.
 
         Returns:
-        (cano_sect, policy, value): Resulting canonical section, policy, value function.
+        [cano_sect, policy, value]: Resulting canonical section, policy, value function.
         True/False: Whether the program successfully converges to a perfect equilibrium.
         nit: Number of iteration steps.
-        record_list=[cano_sect, policy_bias, regret_bias, projgrad, dp_res]: Resulting norm of canonical section, (primal-dual) policy bias, (primal-dual) regret bias, projected gradient, dynamic programming residual.
-        np.vstack(self.record): The ndarray recording every iteration step. (Empty if not recorded.)
+        record_list=[cano_sect, policy_bias, projgrad, dp_res]: Resulting norm of canonical section, (primal-dual) policy bias, projected gradient, dynamic programming residual.
+        np.vstack(self.record): The ndarray recording every iteration step. (None if not recorded.)
         """
+        init_barr, projgrad_stepln, dynaprogr_stepln, tangent_stepln, maxsubnit = 1e3, 2e-4, 1e-1, 2e-2, 10000
         policy = torch.from_numpy(policy).to(torch.get_default_device())
         torch.set_printoptions(precision=preci, sci_mode=True)
         self.Nn = policy.shape[0]
         Nn, Ns, Ni, Na = self.Nn, self.Ns, self.Ni, self.Na
-        barr, value = policy, torch.ones((Nn, Ns, Ni))*self.max_value
+        barr, value = policy*init_barr, torch.ones((Nn, Ns, Ni))*self.max_value
         verbose = verbose if record_file else 0
         if verbose >= 1:
             open(record_file, 'w')
             with open(record_file, 'a') as fio:
                 nlen = (6+preci)*Nn*Ns+(Ns-1)*Nn+2*Nn+Nn+1
-                fio.writelines(f"|{'nit':^7}|{'canosetol':^{6+preci}}|{'cano_sect':^{nlen}}|{'policy_bias':^{nlen}}|{'regret_bias':^{nlen}}|{'projgrad':^{nlen}}|{'dp_res':^{(6+preci)*Nn*Ni+(Ni-1)*Nn+2*Nn+Nn+1}}|{'singuavoi':^{6+preci}}|\n")
-        nit, singu_avoi, pre_projgrad, _canosect_TOL = 0, singuavoi_stepln, torch.zeros((Nn, Ns, Ni, Na)), 1e-2
+                fio.writelines(f"|{'nit':^7}|{'canosetol':^{6+preci}}|{'cano_sect':^{nlen}}|{'policy_bias':^{nlen}}|{'projgrad':^{nlen}}|{'dp_res':^{(6+preci)*Nn*Ni+(Ni-1)*Nn+2*Nn+Nn+1}}|{'singuavoi':^{6+preci}}|\n")
+        nit, pre_projgrad, _canosect_TOL, _singuavoi = 0, 0, 1e-2, 1.0
         while True:
-            subnit, adam_m, adam_v = 0, torch.zeros((Nn, Ns, Ni, Na)), torch.zeros((Nn, Ns, Ni, Na))
+            subnit, adam_m, adam_v = 0, 0, 0
             while True:
-                projgrad, policy_bias, regret_bias, regret, dp_res, piU_jac, cano_sect = self.onto_equilbundl(barr, policy, value)
-                record_list = [torch.linalg.norm(item, float('inf'), dim=-1).max(dim=-1).values for item in [cano_sect, policy_bias, regret_bias, projgrad]]+[dp_res.max(dim=1).values-dp_res.min(dim=1).values]
-                cano_sect_norm, policy_biasnorm, regret_biasnorm, projgrad_norm, value_resangle = record_list
+                projgrad, policy_bias, regret, dp_res, comat11, cano_sect = self.onto_equilbundl(barr, policy, value)
+                record_list = [torch.linalg.norm(item, float('inf'), dim=-1).max(dim=-1).values for item in [cano_sect, policy_bias, projgrad]]+[dp_res.max(dim=1).values-dp_res.min(dim=1).values]
+                cano_sect_norm, policy_biasnorm, projgrad_norm, value_resangle = record_list
+                def unbiased(tol): return (policy_biasnorm < tol).all() and (value_resangle < tol).all()
 
                 if plots and Ns == 2 and Ni == 2 and Na == 2:
                     self.record.append(torch.cat([barr.reshape((Nn, Ns, Ni*Na)), policy.reshape((Nn, Ns, Ni*Na)), regret.reshape((Nn, Ns, Ni*Na)), value], dim=-1).flatten())
                 if verbose >= 2:
-                    with open(record_file, 'a') as fio:
-                        fio.writelines(f"|{subnit:^7d}|{_canosect_TOL:^.{preci}e}|{'|'.join([arr2str(item) for item in record_list])}|{singu_avoi:^.{preci}e}|\n")
+                    print(f"|{subnit:^7d}|{_canosect_TOL:^.{preci}e}|{'|'.join([arr2str(item) for item in record_list])}|{_singuavoi:^.{preci}e}|")
 
                 if (nit := nit+1) > maxnit:
                     return [item.cpu().numpy() for item in [cano_sect, policy, value]], False, nit, [item.cpu().numpy() for item in record_list], torch.vstack(self.record).cpu().numpy() if self.record else None
-                elif (subnit := subnit+1) > maxsubnit or torch.abs(pre_projgrad-projgrad).max() < 1e-12:
-                    if (policy_biasnorm < pdbias_TOL).all() and (regret_biasnorm < pdbias_TOL).all() and (value_resangle < resolu).all():
-                        singu_avoi *= singuavoi_stepln_decay
+                elif (subnit := subnit+1) > maxsubnit or unbiased(1e-9) or torch.abs(pre_projgrad-(pre_projgrad := projgrad)).max() < 1e-12:
+                    if unbiased(3e-8):
+                        _singuavoi *= 0.2
                         if (cano_sect_norm < _canosect_TOL).all():
                             if _canosect_TOL < canosect_TOL:
                                 return [item.cpu().numpy() for item in [cano_sect, policy, value]], True, nit, [item.cpu().numpy() for item in record_list], torch.vstack(self.record).cpu().numpy() if self.record else None
                             else:
-                                _canosect_TOL *= 0.9
+                                _canosect_TOL *= 1-canosect_stepln
                     else:
-                        singu_avoi += singuavoi_stepln
-                        barr, policy, value, piU_jac, cano_sect = bpv_bkp
+                        if not unbiased(1e-6):
+                            barr, policy, value, regret, comat11 = bpv_bkp
+                        _singuavoi += 1
                     break
 
-                pre_projgrad = projgrad
-                adam_m, adam_v = (lambda exp_projgrad: (adam_m+1e-1*(exp_projgrad-adam_m), adam_v+1e-3*(exp_projgrad**2-adam_v)))(projgrad/policy)
+                adam_m += 1e-1*(projgrad-adam_m)
+                adam_v += 1e-3*(projgrad**2-adam_v)
                 dpolicy = projgrad_stepln*adam_m/(adam_v+4*torch.finfo(float).eps)**0.5
-                policy = (lambda vec: vec/vec.sum(dim=-1, keepdims=True))(torch.exp(torch.log(policy)-torch.where(((policy_biasnorm < resolu) & (regret_biasnorm < resolu))[..., None, None], 0, dpolicy)))
+                policy = (lambda vec: vec/vec.sum(dim=-1, keepdims=True))(torch.exp(torch.log(policy)-torch.where((policy_biasnorm < 1e-10)[..., None, None], 0, dpolicy)))
                 value += dynaprogr_stepln*dp_res
 
-            bpv_bkp = barr.clone(), policy.clone(), value.clone(), piU_jac.clone(), cano_sect.clone()
+            bpv_bkp = barr.clone(), policy.clone(), value.clone(), regret.clone(), comat11.clone()
+            diff = self.along_equilbundl(policy, regret, comat11)
+            diff_ln = torch.linalg.norm(policy[..., None]*diff.sum(dim=-1), dim=-2).max(dim=-2).values
+            _canosect_stepln = (tangent_stepln/diff_ln).clip(max=canosect_stepln)/(1+_singuavoi)
+            barr, dbarr = (lambda barr_next: (barr_next, 1-barr_next/barr))(((1-_canosect_stepln)[..., None]*barr+_singuavoi*singuavoi_stepln*policy).clip(min=_canosect_TOL*0.2))
+            policy = (lambda vec: vec/vec.sum(dim=-1, keepdims=True))(torch.exp(torch.log(policy)-torch.einsum('nsiakl,nskl->nsia', diff, dbarr)))
             if verbose >= 1:
                 with open(record_file, 'a') as fio:
-                    fio.writelines(f"|{nit:^7d}|{_canosect_TOL:^.{preci}e}|{'|'.join([arr2str(item) for item in record_list])}|{singu_avoi:^.{preci}e}|\n")
-
-            diff = self.along_equilbundl(barr, policy, piU_jac)
-            barr, dbarr = (lambda barr_next: (barr_next, 1-barr_next/barr))(((1-canosect_stepln)*barr+singu_avoi*policy).clip(min=_canosect_TOL*0.2))
-            policy = (lambda vec: vec/vec.sum(dim=-1, keepdims=True))(torch.exp(torch.log(policy)-torch.einsum('nsiakl,nskl->nsia', diff, dbarr)))
+                    fio.writelines(f"|{nit:^7d}|{_canosect_TOL:^.{preci}e}|{'|'.join([arr2str(item) for item in record_list])}|{_singuavoi:^.{preci}e}|\n")
 
     def onto_equilbundl(self, barr, policy, value):
         Nn, Ns, Ni, Na = self.Nn, self.Ns, self.Ni, self.Na
         piU_jac, piU_vec = self.expected_util(policy, value)
         _piU_vec_max, _cano_sect = (lambda _piU_vec_max: (_piU_vec_max, _piU_vec_max[..., None]-piU_vec))(piU_vec.max(dim=-1).values)
-        value_static = self._brentq(lambda v: (barr/(v[..., None]+_cano_sect)).sum(dim=-1)-1, torch.zeros((Nn, Ns, Ni)), torch.full((Nn, Ns, Ni), 1e3))+_piU_vec_max
+        value_static = self._brentq(lambda v: (barr/(v[..., None]+_cano_sect)).sum(dim=-1)-1, torch.zeros((Nn, Ns, Ni)), Na*barr.max(axis=-1).values)+_piU_vec_max
         regret = value_static[..., None]-piU_vec
-        policy_bias, regret_bias = policy-barr/regret, regret-barr/policy
-        projgrad = (lambda grad: grad-(grad.sum(dim=-1)/Na)[..., None])(regret_bias-torch.einsum('nsab,nsa->nsb', piU_jac, policy_bias.reshape((Nn, Ns, Ni*Na))).reshape((Nn, Ns, Ni, Na)))
+        policy_bias = policy-barr/regret
+        comat11 = torch.eye(Ni*Na)-piU_jac*policy.reshape((Nn, Ns, 1, Ni*Na))/regret.reshape((Nn, Ns, Ni*Na, 1))
+        projgrad = (lambda grad: grad-grad.sum(dim=-1, keepdims=True)*policy)(torch.einsum('nsab,nsa->nsb', comat11, (policy_bias*regret).reshape((Nn, Ns, Ni*Na))).reshape((Nn, Ns, Ni, Na)))
         dp_res = value_static-torch.einsum('nsia,nsia->nsi', policy, regret)-value
-        return projgrad, policy_bias, regret_bias, regret, dp_res, piU_jac, policy*_cano_sect
+        return projgrad, policy_bias, regret, dp_res, comat11, policy*_cano_sect
 
-    def along_equilbundl(self, barr, policy, piU_jac):
+    def along_equilbundl(self, policy, regret, comat11):
         Nn, Ns, Ni, Na = self.Nn, self.Ns, self.Ni, self.Na
-        _policy = policy.reshape((Nn, Ns, Ni*Na))
-        policy_barr = (policy/barr).reshape((Nn, Ns, Ni*Na))
-        comat11 = torch.eye(Ni*Na)-piU_jac*_policy[:, :, None, :]*policy_barr[:, :, :, None]
-        comat21 = torch.kron(torch.eye(Ni), torch.ones(Na))[None, ...]*_policy[:, :, None, :]
-        comat12 = torch.kron(torch.eye(Ni), torch.ones((Na, 1)))[None, ...]*policy_barr[:, :, :, None]
+        comat21 = torch.kron(torch.eye(Ni), torch.ones((1, Na)))*policy.reshape((Nn, Ns, 1, Ni*Na))
+        comat12 = torch.kron(torch.eye(Ni), torch.ones((Na, 1)))/regret.reshape((Nn, Ns, Ni*Na, 1))
         comat = torch.cat([torch.cat([comat11, comat12], dim=-1), torch.cat([comat21, torch.zeros((Nn, Ns, Ni, Ni))], dim=-1)], dim=-2)
         diff = torch.linalg.solve(comat, torch.cat([torch.eye(Ni*Na), torch.zeros((Ni, Ni*Na))], dim=-2)[None, None, ...])[..., :Ni*Na, :].reshape((Nn, Ns, Ni, Na, Ni, Na))
         return diff
@@ -129,6 +129,7 @@ class Game:
 
             The method also applies to model-free cases, where piU_jac and piU_vec are estimated from samples as an agent interacts with a game instance.
         """
+        syms = 'abcdefghjklmopqrtuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
         Nn, Ns, Ni, Na = self.Nn, self.Ns, self.Ni, self.Na
         gamma, ua, Ta = self.model
         ustatic = ua[..., None, :]+gamma*torch.tensordot(Ta, value+self.max_value, dims=([-1], [-2]))
